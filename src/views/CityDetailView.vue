f927c766458eb040564c2821909f28fe9568dda8
djdongjae <jk47wee@gmail.com>
2024-06-29 07:44:45 +0900

    Refactor: 메인페이지 디자인 변경

--- src/views/CityDetailView.vue
+++ src/views/CityDetailView.vue
@@ -1,202 +1,272 @@
 <template>
-    <div id="CityDetail">
-      <div class="city-header">
-        <img :src="basic.cityImage" alt="city.name" class="city-image">
-        <div class="city-header-text">
-          <h1 class="city-name">{{ basic.name }}</h1>
-          <p class="city-description">{{ basic.info }}</p>
-        </div>
+  <div id="CityDetail">
+    <div class="city-header">
+      <img :src="basic.cityImage" alt="city.name" class="city-image">
+      <div class="city-header-text">
+        <h1 class="city-name">{{ basic.name }}</h1>
+        <p class="city-description">{{ basic.info }}</p>
       </div>
-      <blockquote class="city-recommendation">{{ basic.recommendation }}</blockquote>
-  
-      <div class="reviews-section">
-        <div class="reviews-header">
-          <h2>📌 추천 명소</h2>
-          <button class="add-review-button" @click="addReview(this.$route.params.id)">등록하기</button>
-        </div>
-        <div v-for="review in reviews" :key="review.reviewId" class="review-card">
-          <div class="review-content">
-            <h3 class="review-title" v-html="review.placeName1"></h3>
-            <p class="review-description" v-html="review.placeInfo1"></p>
-            <img :src="review.placeImage1" alt="review.placeName1" class="review-image">
-          </div>
-        </div>
+    </div>
+    <blockquote class="city-recommendation">{{ basic.recommendation }}</blockquote>
+
+    <div class="reviews-section">
+      <div class="reviews-header">
+        <h2>📌 추천 명소</h2>
+        <button class="add-review-button" @click="addReview(this.$route.params.id)">등록하기</button>
       </div>
-    
-      <div class="blogs-section">
-        <h2>📝 네이버 블로그</h2>
-        <div v-for="blog in blogs" :key="blog.title" class="blog-card" @click="goBlog(blog.link)">
-          <div class="blog-content">
-            <p class="blog-title" v-html="blog.title"></p>
-            <p class="blog-description" v-html="blog.description"></p>
-            <p class="blogger-name">{{ blog.bloggername }}</p>
-          </div>
+      <div v-for="review in reviews" :key="review.reviewId" class="review-card" @click="openModal(review)">
+        <h3 class="review-title">{{ truncateText(review.placeName1, 10) }}</h3>
+        <img :src="review.placeImage1" alt="review.placeName1" class="review-image">
+      </div>
+    </div>
+
+    <div class="blogs-section">
+      <h2>📝 네이버 블로그</h2>
+      <div v-for="blog in blogs" :key="blog.title" class="blog-card" @click="goBlog(blog.link)">
+        <div class="blog-content">
+          <p class="blog-title" v-html="blog.title"></p>
+          <p class="blog-description" v-html="blog.description"></p>
+          <p class="blogger-name">{{ blog.bloggername }}</p>
         </div>
       </div>
     </div>
-  </template>
-  
-  <script>
-  import { loadCity, loadNaverBlog } from "../apis/cityService";
-  import { loadReviews } from "../apis/reviewService";
-  import { checkLogin } from "../apis/authService";
-  
-  export default {
-    name: "CityDetailView",
-    data() {
-      return {
-        basic: {},
-        blogs: [],
-        reviews: []
-      };
+
+    <div v-if="showModal" class="modal" @click.self="closeModal">
+      <div class="modal-content">
+        <span class="close" @click="closeModal">&times;</span>
+        <h2>{{ selectedReview.placeName1 }}</h2>
+        <p>{{ selectedReview.placeInfo1 }}</p>
+        <img :src="selectedReview.placeImage1" alt="selectedReview.placeName1" class="modal-image">
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { loadCity, loadNaverBlog } from "../apis/cityService";
+import { loadReviews } from "../apis/reviewService";
+import { checkLogin } from "../apis/authService";
+
+export default {
+  name: "CityDetailView",
+  data() {
+    return {
+      basic: {},
+      blogs: [],
+      reviews: [],
+      showModal: false,
+      selectedReview: {}
+    };
+  },
+  async mounted() {
+    const id = this.$route.params.id;
+    this.basic = await loadCity(id);
+    const query = this.basic.name;
+    this.blogs = await loadNaverBlog(query);
+    this.reviews = await loadReviews(id);
+  },
+  methods: {
+    goBlog(link) {
+      window.location = link;
     },
-    async mounted() {
-      const id = this.$route.params.id;
-      this.basic = await loadCity(id);
-      const query = this.basic.name;
-      this.blogs = await loadNaverBlog(query);
-      this.reviews = await loadReviews(id);
-    },
-    methods: {
-      goBlog(link) {
-        window.location = link;
-      },
-      addReview(id) {
-        if (checkLogin()) {
-          this.$router.push("/addReview/" + id);
-        } else {
-          alert("로그인이 필요한 서비스입니다.");
-          this.$router.push("/login");
-        }
+    addReview(id) {
+      if (checkLogin()) {
+        this.$router.push("/addReview/" + id);
+      } else {
+        alert("로그인이 필요한 서비스입니다.");
+        this.$router.push("/login");
       }
+    },
+    truncateText(text, length) {
+      if (text.length > length) {
+        return text.substring(0, length) + '...';
+      }
+      return text;
+    },
+    openModal(review) {
+      this.selectedReview = review;
+      this.showModal = true;
+    },
+    closeModal() {
+      this.showModal = false;
     }
   }
-  </script>
-  
-  <style scoped>
-  #CityDetail {
-    padding: 20px;
-    font-family: 'Pretendard', sans-serif;
-  }
-  
-  .city-header {
-    display: flex;
-    align-items: center;
-    margin-bottom: 20px;
-  }
-  
-  .city-image {
-    width: 150px;
-    height: 150px;
-    border-radius: 10px;
-    margin-right: 20px;
-    object-fit: cover;
-  }
-  
-  .city-header-text {
-    display: flex;
-    flex-direction: column;
-  }
-  
-  .city-name {
-    font-size: 28px;
-    font-weight: bold;
-    margin: 5px 0;
-  }
-  
-  .city-description {
-    font-size: 16px;
-    color: #777;
-  }
-  
-  .city-recommendation {
-    font-size: 16px;
-    color: #555;
-    margin: 10px;
-    padding: 10px 20px;
-    border-left: 4px solid #ff6f61; /* 인용문구 스타일 */
-    background-color: #f9f9f9;
-    font-style: italic;
-  }
-  
-  .blogs-section, .reviews-section {
-    margin-top: 20px;
-  }
-  
-  .reviews-header {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    margin-bottom: 10px;
-  }
-  
-  .blogs-section h2, .reviews-section h2 {
-    margin-bottom: 10px;
-    color: #ff6f61;
-    display: flex;
-    align-items: center;
-    font-size: 22px;
-    font-weight: bold;
-  }
-  
-  .blog-card, .review-card {
-    padding: 15px;
-    border: 1px solid #eee;
-    border-radius: 10px;
-    background-color: #fff;
-    margin-bottom: 15px;
-    transition: box-shadow 0.3s ease-in-out;
-    cursor: pointer;
-  }
-  
-  .blog-card:hover, .review-card:hover {
-    box-shadow: 3px 3px 12px rgba(0, 0, 0, 0.1);
-  }
-  
-  .blog-content, .review-content {
-    display: flex;
-    flex-direction: column;
-  }
-  
-  .blog-title, .review-title {
-    font-size: 18px;
-    font-weight: bold;
-    margin: 0 0 5px;
-  }
-  
-  .blog-description, .review-description {
-    font-size: 14px;
-    color: #555;
-    margin: 5px 0;
-  }
-  
-  .blogger-name {
-    font-size: 12px;
-    color: #999;
-  }
-  
-  .review-image {
-    width: 100%;
-    height: auto;
-    border-radius: 10px;
-    margin-top: 10px;
-  }
-  
-  .add-review-button {
-    display: inline-block;
-    padding: 8px 10px;
-    font-size: 13px;
-    font-weight: bold;
-    color: #fff;
-    background-color: #333; /* 옅은 검은색 버튼 */
-    border: none;
-    border-radius: 5px;
-    cursor: pointer;
-    transition: background-color 0.3s ease;
-  }
-  
-  .add-review-button:hover {
-    background-color: #555; /* 호버 시 색상 변경 */
-  }
-  </style>
-  
+}
+</script>
+
+<style scoped>
+#CityDetail {
+  padding: 20px;
+  font-family: 'Pretendard', sans-serif;
+}
+
+.city-header {
+  display: flex;
+  align-items: center;
+  margin-bottom: 20px;
+}
+
+.city-image {
+  width: 150px;
+  height: 150px;
+  border-radius: 10px;
+  margin-right: 20px;
+  object-fit: cover;
+}
+
+.city-header-text {
+  display: flex;
+  flex-direction: column;
+}
+
+.city-name {
+  font-size: 28px;
+  font-weight: bold;
+  margin: 5px 0;
+}
+
+.city-description {
+  font-size: 16px;
+  color: #777;
+}
+
+.city-recommendation {
+  font-size: 16px;
+  color: #555;
+  margin: 10px;
+  padding: 10px 20px;
+  border-left: 4px solid #ff6f61;
+  background-color: #f9f9f9;
+  font-style: italic;
+}
+
+.blogs-section, .reviews-section {
+  margin-top: 20px;
+}
+
+.reviews-header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 10px;
+}
+
+.blogs-section h2, .reviews-section h2 {
+  margin-bottom: 10px;
+  color: #ff6f61;
+  display: flex;
+  align-items: center;
+  font-size: 22px;
+  font-weight: bold;
+}
+
+.blog-card, .review-card {
+  padding: 15px;
+  border: 1px solid #eee;
+  border-radius: 10px;
+  background-color: #fff;
+  margin-bottom: 15px;
+  transition: box-shadow 0.3s ease-in-out;
+  cursor: pointer;
+  overflow: hidden;
+}
+
+.blog-card:hover, .review-card:hover {
+  box-shadow: 3px 3px 12px rgba(0, 0, 0, 0.1);
+}
+
+.blog-content, .review-content {
+  display: flex;
+  flex-direction: column;
+  max-width: 100%;
+}
+
+.blog-title, .review-title {
+  font-size: 18px;
+  font-weight: bold;
+  margin: 0 0 5px;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  max-width: 100%;
+}
+
+.review-image {
+  width: 100%;
+  height: auto;
+  border-radius: 10px;
+  margin-top: 10px;
+}
+
+.blogger-name {
+  font-size: 12px;
+  color: #999;
+}
+
+.add-review-button {
+  display: inline-block;
+  padding: 8px 10px;
+  font-size: 13px;
+  font-weight: bold;
+  color: #fff;
+  background-color: #333;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  transition: background-color 0.3s ease;
+}
+
+.add-review-button:hover {
+  background-color: #555;
+}
+
+.modal {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: fixed;
+  z-index: 1000;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  overflow: auto;
+  background-color: rgba(0, 0, 0, 0.5);
+}
+
+.modal-content {
+  background-color: #fff;
+  padding: 20px;
+  border: 1px solid #888;
+  width: 80%;
+  max-width: 600px;
+  border-radius: 10px;
+  text-align: center;
+  overflow-y: auto;
+  max-height: 90%;
+  word-wrap: break-word;
+  overflow-wrap: break-word;
+}
+
+.close {
+  color: #aaa;
+  float: right;
+  font-size: 28px;
+  font-weight: bold;
+}
+
+.close:hover,
+.close:focus {
+  color: black;
+  text-decoration: none;
+  cursor: pointer;
+}
+
+.modal-image {
+  width: 100%;
+  max-width: 400px;
+  height: auto;
+  border-radius: 10px;
+  margin-top: 10px;
+}
+</style>
--- src/views/CountryListView.vue
+++ src/views/CountryListView.vue
@@ -1,16 +1,20 @@
 <template>
   <div id="countryListView">
     <div ref="wordCloudContainer" class="word-cloud-container"></div>
-    <h4>여행지 정보를 알아보아요</h4>
+    <div class="logo-section">
+      <h1 class="service-name">Travel Compass</h1>
+    </div>
     <div class="country-container">
       <div
-        v-for="country in countries"
-        :key="country.countryId"
-        @click="goEdit(country.countryId)"
-        class="country-item"
+          v-for="country in countries"
+          :key="country.countryId"
+          @click="goEdit(country.countryId)"
+          class="country-item"
       >
-        <img :src="country.countryImage" alt="Country Image" />
-        <div>{{ country.name }}</div>
+        <div class="image-container">
+          <img :src="country.countryImage" alt="Country Image" />
+        </div>
+        <div class="country-name">{{ country.name }}</div>
       </div>
     </div>
   </div>
@@ -35,13 +39,21 @@ export default {
     this.renderWordCloud();
   },
   methods: {
-    // 워드 클라우드 값을 받아온 다음 배열에 있는 값을 하나씩 가져옴.
     renderWordCloud() {
       const wordList = this.words.map((word) => [word.text, word.value]);
-      WordCloud(this.$refs.wordCloudContainer, {
+      const canvas = this.$refs.wordCloudContainer;
+      WordCloud(canvas, {
         list: wordList,
-        weightFactor: 10, // value에 곱해지는 인수, 값이 클수록 글씨가 커짐.
-        minSize: 2, // 최소 글씨 크기
+        weightFactor: 13, // 글자 크기 조정
+        minSize: 5,
+        fontFamily: 'Pretendard',
+        fontWeight: 900,
+        color: 'random-dark',
+        backgroundColor: '#d9d6d6',
+        rotateRatio: 0.5,
+        shape: 'circle', // 'circle' 모양을 사용합니다
+        gridSize: 2, // gridSize를 작게 설정하여 촘촘하게
+        drawOutOfBound: false,
       });
     },
     goEdit(countryId) {
@@ -52,9 +64,16 @@ export default {
 </script>
 
 <style scoped>
+h1{
+  font-size: 40px;
+  color: #555;
+}
+
 img {
-  width: 20px;
-  border-radius: 5px;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  border-radius: 50%;
 }
 
 h4 {
@@ -62,47 +81,84 @@ h4 {
   background-color: rgb(255, 200, 98);
   border-radius: 10px;
   padding: 10px;
+  margin-bottom: 20px;
+  font-family: 'Pretendard', sans-serif;
 }
 
 #countryListView {
   margin: auto;
   padding: 20px;
-  width: 80%;
-  right: 8px;
+  width: 90%;
+}
+
+.logo-section {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-bottom: 30px;
+}
+
+.logo {
+  width: 35px;
+  height: auto;
+}
+
+.service-name {
+  font-size: 30px;
+  font-family: 'Pretendard', sans-serif;
+  margin: 0;
 }
 
 .word-cloud-container {
   width: 100%;
-  height: 200px;
-  border: 1px solid #ddd;
-  border-radius: 20px;
-  width: 100%;
-  margin: auto;
+  height: 300px;
+  border-radius: 200px;
+  margin-top: 20px;
+  margin-bottom: 20px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
 }
 
 .country-container {
   display: flex;
   flex-wrap: wrap;
-  gap: 20px;
-  justify-content: center;
+  gap: 10px;
+  justify-content: space-between;
 }
 
 .country-item {
-  flex: 0 1 calc(25% - 15px);
-  border: 1px solid rgb(179, 175, 175);
-  border-radius: 10px;
-  box-sizing: border-box;
+  width: 50px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
   text-align: center;
   cursor: pointer;
   background-color: #f0f0f0;
-  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-  transition: transform 0.2s;
-  font-size: 10px;
-  padding: 5px;
+  padding: 10px;
+  border-radius: 10px;
+  transition: transform 0.2s, box-shadow 0.2s;
 }
 
 .country-item:hover {
-  background-color: #ddd;
   transform: translateY(-5px);
+  box-shadow: 0 8px 16px rgba(0, 0, 0, 0.2);
+}
+
+.image-container {
+  width: 50px;
+  height: 50px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-bottom: 5px;
+  border-radius: 50%;
+  overflow: hidden;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+}
+
+.country-name {
+  font-size: 12px;
+  font-weight: 600;
+  color: #333;
+  font-family: 'Pretendard', sans-serif;
 }
 </style>
