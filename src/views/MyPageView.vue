c8267bbce7df4b9a2c03cf4f609c53f636ff9483
Sungpie <sungsooben@naver.com>
2024-06-29 22:13:15 +0900

    Feat: 마이 페이지 기본 정보, 메인 페이지 챗GPT 기능 추가

--- src/views/MyPageView.vue
+++ src/views/MyPageView.vue
@@ -1,16 +1,79 @@
 <template>
   <div>
     <h1>마이페이지</h1>
+    <div class="welcome">
+      <img
+        :src="userInfo.pictureUrl"
+        class="welcome-image"
+        alt="User Picture"
+      />
+      <div class="welcome-right">
+        <h3 class="welcome-right-name">{{ userInfo.name }}님, 반갑습니다.</h3>
+        <p>{{ userInfo.email }}</p>
+      </div>
+    </div>
+    <section>
+      <h4>내가 올린 글</h4>
+    </section>
   </div>
 </template>
 
 <script>
+import { getUserInfo } from "../apis/authService";
+
 export default {
   name: "MyPageView",
-  methods: {
-    async getUser() {
-
-    }
-  }
-}
+  data() {
+    return {
+      userInfo: {},
+      userReview: [],
+    };
+  },
+  async mounted() {
+    this.userInfo = await getUserInfo();
+  },
+  methods: {},
+};
 </script>
+
+<style scoped>
+h1 {
+  text-align: center;
+}
+
+.welcome {
+  display: flex;
+  border-top: 4px solid rgb(201, 200, 200);
+  border-bottom: 4px solid rgb(201, 200, 200);
+  justify-content: space-evenly;
+  border-radius: 10px;
+  width: 90%;
+  margin: auto;
+}
+
+.welcome-image {
+  width: 20%;
+  height: 20%;
+  border-radius: 50%;
+  margin-right: 10px;
+  margin-top: 17px;
+  margin-left: 5px;
+}
+
+.welcome-right {
+  margin-bottom: 13px;
+}
+
+.welcome-right-name {
+  margin-top: 27px;
+  margin-bottom: -13px;
+}
+
+p {
+  color: gray;
+}
+
+h4 {
+  text-align: center;
+}
+</style>
